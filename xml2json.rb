7d992a2b364f0a305f72eef2275c8733a4330bb9
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2014-01-31 18:51:57 +0100

    Parses simple xml into ruby hashes

---
+++ xml2json.rb
@@ -0,0 +1,55 @@
+require 'rspec/autorun'
+require 'nokogiri'
+
+describe "xml2json" do
+  it "parses xml into json" do
+    expect(xml2json('<a><b>Hello</b></a>')).to eq({ "a" => { "b" => "Hello" } })
+    expect(xml2json('<a><b>Hello</b><c>World</c></a>')).to(
+      eq({ "a" => { "b" => "Hello", "c" => "World" } })
+    )
+
+    expect(xml2json('<a><b><c>World</c></b></a>')).to(
+      eq({ "a" => { "b" => { "c" => "World" } } })
+    )
+
+    expect(xml2json('<a><b><c>Hello</c><d>World</d></b></a>')).to(
+      eq({ "a" => { "b" => { "c" => "Hello", "d" => "World" } } })
+    )
+
+    expect(xml2json('<a><b><c><d>World</d></c></b></a>')).to(
+      eq({ "a" => { "b" => { "c" => { "d" => "World" } } } })
+    )
+
+    expect(xml2json('<a><b><c><d>Hello</d><e>World</e></c></b></a>')).to(
+      eq({ "a" => { "b" => { "c" => { "d" => "Hello", "e" => "World" } } } })
+    )
+
+    expect(xml2json('<a><b><x>Io</x><c><d>Hello</d><e>World</e></c></b></a>')).to(
+      eq({ "a" => { "b" => { "x" => "Io", "c" => { "d" => "Hello", "e" => "World" } } } })
+    )
+  end
+end
+
+def xml2json xml
+  doc = Nokogiri.XML(xml)
+  hash = { doc.root.name => {} }
+
+  doc.root.element_children.each do |node|
+    hash[doc.root.name][node.name] = if node.element_children.count == 0
+      node.text
+    else
+      node2json(node)
+    end
+  end
+  hash
+end
+
+def node2json node
+  node.element_children.each_with_object({}) do |child, hash|
+    if child.element_children.count > 0
+      hash[child.name] = node2json(child)
+    else
+      hash[child.name] = child.text
+    end
+  end
+end
