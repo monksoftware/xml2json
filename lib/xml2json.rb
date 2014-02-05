bb10229ae689383a07687c8b904fa39ae0450180
Andrea D'Ippolito <adedip@gmail.com>
2014-02-05 14:00:34 +0100

    refactoring

--- lib/xml2json.rb
+++ lib/xml2json.rb
@@ -8,35 +8,37 @@ module XML2JSON
 
   def self.node2json node
     node.element_children.each_with_object({}) do |child, hash|
+      
       has_children = child.element_children.count > 0
+
       if hash.has_key?(child.name)
         tmp = hash[child.name]
         hash[child.name] = []
         hash[child.name] << tmp
-        if child.attributes.empty?
-          hash[child.name] << (has_children ? node2json(child) : child.text)
+        
+        if has_children
+          hash[child.name] << parse_attributes(child).merge(node2json(child))
         else
-          if has_children
-            hash[child.name] << parse_attributes(child).merge(node2json(child))
-          else
-            hash[child.name] << parse_attributes(child).merge({"_text" => child.text})
-          end
+          hash[child.name] << (child.attributes.empty? ? child.text : parse_attributes(child).merge({"_text" => child.text}))
         end
+
       else
-        if child.attributes.empty?
-          hash[child.name] = (has_children ? node2json(child) : child.text)
+        
+        if has_children
+          hash[child.name] = parse_attributes(child).merge(node2json(child))
         else
-          if has_children
-            hash[child.name] = parse_attributes(child).merge(node2json(child))
-          else
-            hash[child.name] = parse_attributes(child).merge({"_text" => child.text})
-          end
+          hash[child.name] = (child.attributes.empty? ? child.text : parse_attributes(child).merge({"_text" => child.text}))
         end
+
       end
     end
   end
 
   def self.parse_attributes(node)
-    {"_attributes" => Hash[node.attributes.map { |k, v| [k, v.value] } ]}
+    if node.attributes.empty?
+      {}
+    else
+      {"_attributes" => Hash[node.attributes.map { |k, v| [k, v.value] } ]}
+    end
   end
 end
